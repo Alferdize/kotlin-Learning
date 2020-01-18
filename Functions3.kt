2a3750f394a5d70b04d144b3f2b99ab7030ec559
Alferdize <simonandrew132@gmail.com>
2020-01-19 01:22:29 +0530

    Functions3

---
+++ Functions3.kt
@@ -0,0 +1,24 @@
+import java.math.*
+import java.util.Random
+
+fun main(args : Array<String>){
+    println("Hello , $args[0]")
+    feedTheFish()
+}
+
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay():String{
+    val week = listOf("Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday")
+    return week[Random().nextInt(7)]
+}
