a7acfc18304ac664dbdc6699f2d4282c28f8a294
Alferdize <simonandrew132@gmail.com>
2020-01-19 00:49:58 +0530

    Array3

--- Array3.kt
+++ Array3.kt
@@ -7,4 +7,5 @@ fun main(args: Array<String>){
     for(i in 3..6 step 2) print(i)
     val array = Array(7){it*it}
     print(array.asList())
+    for(i in 0..100 step 7) println(i.String() + "-")
 }
