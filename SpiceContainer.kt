9e2bc6218b2a3db07d25193f4d62e63327e230e4
Alferdize <simonandrew132@gmail.com>
2020-01-20 00:38:21 +0530

    Final

---
+++ SpiceContainer.kt
@@ -0,0 +1,38 @@
+abstract class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor) : SpiceColor by color {
+    abstract fun prepareSpice()
+}
+
+
+class Curry(name: String, spiciness: String,
+            color: SpiceColor = YellowSpiceColor) : Spice(name, spiciness, color), Grinder {
+    override fun grind() {
+    }
+
+    override fun prepareSpice() {
+        grind()
+    }
+}
+
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: String
+}
+
+object YellowSpiceColor : SpiceColor {
+    override val color = "Yellow"
+}
+
+
+data class SpiceContainer(var spice: Spice) {
+    val label = spice.name
+}
+fun main(){
+val spiceCabinet = listOf(SpiceContainer(Curry("Yellow Curry", "mild")),
+        SpiceContainer(Curry("Red Curry", "medium")),
+        SpiceContainer(Curry("Green Curry", "spicy")))
+
+for(element in spiceCabinet) println(element.label)
+}
